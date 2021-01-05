8ab15de71eb7cae164b271c6bf7ad9aa82ac22dd
Jonathan Langlois <jon@button.is>
2021-01-05 14:05:32 -0800

    chore: linting

--- app/jest.config.js
+++ app/jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   moduleNameMapper: { '^.+\\.(css|less)$': '<rootDir>/tests/CSSStub.js' },
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/backend/', '<rootDir>/cypress/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['<rootDir>/node_modules/babel-jest', { "configFile": "./babel.config.test.json" }],
+    '^.+\\.(js|jsx|ts|tsx)$': ['<rootDir>/node_modules/babel-jest', { configFile: './babel.config.test.json' }],
   },
 };
