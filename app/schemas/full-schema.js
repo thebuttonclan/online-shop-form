ed2e4725869f4fed4d721c364f2da2b1b8037485
Michael Vesprini <mike@button.is>
2021-01-07 15:54:26 -0800

    refactor(full schema): full schema is now a func that will merge the individual page schemas

---
+++ app/schemas/full-schema.js
@@ -0,0 +1,10 @@
+import schema1 from './page-1';
+import schema2 from './page-2';
+
+function mergeIfArray(objValue, srcValue) {
+  if (_.isArray(objValue)) {
+    return objValue.concat(srcValue);
+  }
+}
+
+const fullSchema = _.mergeWith(schema1, schema2, mergeIfArray);
