2bd004a616b071d1b19bec8e9a62ae3060fd527d
Jonathan Langlois <jon@button.is>
2021-01-07 15:54:42 -0800

    chore: remove local state
    remove local state from _app.js
    
    10

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -4,8 +4,6 @@ import DefaultLayout from 'layouts/DefaultLayout';
 import { createGlobalStyle } from 'styled-components';
 import 'semantic-ui-css/semantic.min.css';
 
-const { version: appVersion } = require('../package.json');
-
 const GlobalStyle = createGlobalStyle`
   .bcgov-bg-color {
     background-color: #036 !important;
@@ -13,21 +11,13 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 class App extends PureComponent {
-  state = {
-    allFormData: { formVersion: appVersion },
-  };
-
-  addFormData = data => {
-    this.setState({ allFormData: { ...this.state.allFormData, ...data } });
-  };
-
   render() {
     const { Component, pageProps, router } = this.props;
     return (
       <>
         <GlobalStyle />
         <DefaultLayout query={{ ...router.query }}>
-          <Component {...pageProps} addFormData={this.addFormData} allFormData={this.state.allFormData} />
+          <Component {...pageProps} />
         </DefaultLayout>
       </>
     );
