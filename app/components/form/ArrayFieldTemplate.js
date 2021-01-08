53f86a5ede27c0ef5eba7ad02de38f3b5051ce8e
Jonathan Langlois <jon@button.is>
2021-01-08 13:28:49 -0800

    feat: delete array
    add delete functionality for custom array template
    
    15

--- app/components/form/ArrayFieldTemplate.js
+++ app/components/form/ArrayFieldTemplate.js
@@ -1,13 +1,15 @@
-import Input from 'components/form/Input';
-import { useEffect } from 'react';
+import React from 'react';
+import { Button, Icon, StepContent } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const SrightContent = styled.div`
+  display: flex;
+  justify-content: right;
+`;
 
 export default function ArrayFieldTemplate(props) {
   const { title, TitleField } = props;
-  useEffect(() => {
-    const nonScripts = Array(document.querySelectorAll('.noscript'));
-    console.log(nonScripts);
-    nonScripts.forEach(el => el.remove && el.remove());
-  }, []);
+
   return (
     <div>
       <TitleField title={title} />
@@ -15,12 +17,25 @@ export default function ArrayFieldTemplate(props) {
         <textarea />
       </noscript>
       {props.items.map(element => {
-        return element.children;
+        return (
+          <>
+            {element.children}
+            <SrightContent className="ui right">
+              <Button className="right" color="red" icon onClick={element.onDropIndexClick(element.index)}>
+                Delete
+                <Icon name="trash" />
+              </Button>
+            </SrightContent>
+          </>
+        );
       })}
       {props.canAdd && typeof window === 'object' && (
-        <button type="button" onClick={props.onAddClick}>
-          Add {title}
-        </button>
+        <div>
+          <Button className="right" color="green" icon onClick={props.onAddClick}>
+            <Icon name="plus" />
+            Add {title}
+          </Button>
+        </div>
       )}
     </div>
   );
