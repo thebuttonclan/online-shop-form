27ab5a48c7bd2d9557eed7fe5eb8b0ff3dbc5023
Junmin Ahn <junmin@button.is>
2021-01-14 13:00:23 -0800

    chore: style error pages

--- app/pages/message/error.js
+++ app/pages/message/error.js
@@ -1,9 +1,55 @@
-import { Container } from 'semantic-ui-react';
+import { Container, Icon } from 'semantic-ui-react';
+import Header2 from 'components/Header2';
+import StyledUl from 'components/StyledUl';
+import styled from 'styled-components';
+import { HUGE_FONT, MIN_PADDING } from 'theme';
+import StyledP from 'components/StyledP';
+import { validateFormData } from 'services/application';
+import pageSchemas from 'schemas/page-schemas';
+import HrefLink from 'components/HrefLink';
+import startCase from 'lodash/startCase';
+
+const IndentedUl = styled(StyledUl)`
+  margin-left: calc(${MIN_PADDING} * 3) !important;
+`;
+
+const SuccessBanner = styled.div`
+  min-height: 150px;
+  background-color: #ef4848;
+  color: white;
+  width: 100%;
+  margin-top: 100px;
+  margin-bottom: ${MIN_PADDING};
+  font-size: ${HUGE_FONT};
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+  padding: 40px;
+
+  @media (max-width: 430px) {
+    & p {
+      text-align: center;
+    }
+  }
+`;
 
 export default function ErrorPage() {
   return (
     <Container>
-      <h1>Application Failed</h1>
+      <SuccessBanner>
+        <p>
+          <Icon name="exclamation circle"></Icon>
+          Something went wrong
+        </p>
+      </SuccessBanner>
+      <Header2>We were unable to process your request.</Header2>
+      <StyledP>
+        <HrefLink href="/">Click here to return to the landing page</HrefLink>
+      </StyledP>
+      <StyledP>
+        If you have any questions about your applications please contact us at:{' '}
+        <a href="mailto:info@strongerbc.ca">info@strongerbc.ca</a>
+      </StyledP>
     </Container>
   );
 }
