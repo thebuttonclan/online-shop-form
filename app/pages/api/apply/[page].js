9f35d489a300a721c8518baf96752ce7270b9459
Junmin Ahn <junmin@button.is>
2021-01-17 09:28:04 -0800

    chore: fix a issue session data wiped on error page

--- app/pages/api/apply/[page].js
+++ app/pages/api/apply/[page].js
@@ -15,16 +15,15 @@ async function handler(req, res) {
   const clearedFormData = removePageFields(formData, currentPageSchema);
 
   // Clean up newly posted data
-  const newData = matchPostBody(postData, currentPageSchema);
-  const allData = { ...clearedFormData, ...newData };
-  session.formData = allData;
-  console.log('Cleaned newData is: ', allData);
+  const clearedPostData = matchPostBody(postData, currentPageSchema);
+  const newFormData = { ...clearedFormData, ...clearedPostData };
+  session.formData = newFormData;
+  console.log('Cleaned newData is: ', newFormData);
 
-  const context = { req, newData, page, js };
+  const context = { req, newData: newFormData, page, js };
 
   if (page === LAST_PAGE) {
     await submitApplication(context);
-    session.formData = {};
   } else {
     pageForward(context);
   }
