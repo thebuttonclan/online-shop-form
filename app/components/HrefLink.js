09039c54a106b0aad31d12a1624d6bfebed61e5a
Junmin Ahn <junmin@button.is>
2021-01-20 13:28:01 -0800

    chore: add global link style to use for HrefLink component

--- app/components/HrefLink.js
+++ app/components/HrefLink.js
@@ -1,18 +1,12 @@
 import Link from 'next/link';
-import styled from 'styled-components';
-
-const Anchor = styled.a`
-  cursor: pointer;
-  &:hover {
-    text-decoration: underline;
-  }
-`;
 
 export default function HrefLink(props) {
   const { children, blank, ...otherProps } = props;
   return (
     <Link {...otherProps} passhref>
-      <Anchor target={blank ? '_blank' : ''}>{children}</Anchor>
+      <a className="link" target={blank ? '_blank' : ''}>
+        {children}
+      </a>
     </Link>
   );
 }
