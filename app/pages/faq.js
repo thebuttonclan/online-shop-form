1e87caed69ffc04f037a59a7f562eee501e78e8c
Jonathan Langlois <jon@button.is>
2021-01-20 11:35:17 -0800

    fix: use contents for indices
    64

--- app/pages/faq.js
+++ app/pages/faq.js
@@ -365,9 +365,7 @@ export default class Faq extends Component {
 
   handleAllClick = () => {
     const { activeIndexs } = this.state;
-    const newIndexes =
-      activeIndexs.length >= 1 ? [] : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-
+    const newIndexes = activeIndexs.length >= 1 ? [] : contents.map((a, i) => i);
     this.setState({ activeIndexs: newIndexes });
   };
 
