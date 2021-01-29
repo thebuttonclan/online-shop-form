32dab41f719bef7bc8d48620fa8d3885a208bd02
Jonathan Langlois <jon@button.is>
2021-01-29 11:19:05 -0800

    chore: add gtm as html

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -33,19 +33,15 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* <!-- Google Tag Manager --> */}
-          <script>
-            {/* eslint-disable */}
-            {(function (w, d, s, l, i) {
-              w[l] = w[l] || [];
-              w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-              var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-              j.async = true;
-              j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-              f.parentNode.insertBefore(j, f);
-            })(window, document, 'script', 'dataLayer', 'GTM-K88HWTJ')}
-          </script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K88HWTJ');`,
+            }}
+          />
           {/* <!-- End Google Tag Manager --> */}
 
           <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -56,14 +52,12 @@ class MyDocument extends Document {
         </Head>
         <body>
           {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-K88HWTJ"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            ></iframe>
-          </noscript>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K88HWTJ"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
           {/* End Google Tag Manager (noscript)  */}
           <Main />
           <NextScript />
