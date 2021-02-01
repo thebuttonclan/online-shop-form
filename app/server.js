ebf738dcbfce7203a315add1924f8abf7073b0a1
Jonathan Langlois <jon@button.is>
2021-02-01 11:01:04 -0800

    chore: http redirect for axure
    99

--- app/server.js
+++ app/server.js
@@ -12,23 +12,6 @@ const domains = ['launchonline.ca', 'dev.launchonline.ca', 'test.launchonline.ca
 app.prepare().then(async () => {
   const expressServer = await initExpresss();
 
-  expressServer.use((req, res, next) => {
-    let hostname = req.hostname;
-
-    domains.forEach(domain => {
-      if (req.hostname === `www.${domain}`) {
-        hostname = domain;
-      } else if (req.hostname === domain) hostname = domain;
-    });
-
-    if (req.headers['x-forwarded-proto'] === 'http') {
-      res.redirect(301, `https://${hostname}${req.url}`);
-      return;
-    }
-
-    next();
-  });
-
   // catch all other routes and return the index file
   expressServer.all('*', async (req, res) => handle(req, res));
 
