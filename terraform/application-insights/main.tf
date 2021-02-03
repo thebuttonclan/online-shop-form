80fc329e52671d81f1c6e5556e4fae1bd10dda18
Junmin Ahn <junmin@button.is>
2021-02-03 10:28:10 -0800

    feat: set up app availability alert on production website

--- terraform/application-insights/main.tf
+++ terraform/application-insights/main.tf
@@ -31,7 +31,7 @@ resource "azurerm_application_insights_web_test" "this" {
   configuration = <<XML
 <WebTest Name="WebTest1" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
   <Items>
-    <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="https://dev.launchonline.ca/" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+    <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="https://launchonline.ca/" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
   </Items>
 </WebTest>
 XML
@@ -76,5 +76,15 @@ resource "azurerm_monitor_action_group" "this" {
   email_receiver {
     name          = "Mike Vesprini"
     email_address = "mike@button.is"
+  }
+
+  email_receiver {
+    name          = "Justyn Maglalang"
+    email_address = "jmaglalang@alacritycanada.com"
+  }
+
+  email_receiver {
+    name          = "Stephanie"
+    email_address = "stephanie@jetstream.agency"
   }
 }
