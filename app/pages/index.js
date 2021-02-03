a04562df1583d74a244a45e80261e5c028bd3843
Michael Vesprini <mike@button.is>
2021-02-03 10:29:30 -0800

    chore: spacing and padding

--- app/pages/index.js
+++ app/pages/index.js
@@ -45,9 +45,6 @@ const HorizontalRule = styled.hr`
 
 const ScrollHeader1 = styled(Header1)`
   scroll-margin-top: ${TOP_HEIGHT};
-`;
-
-const ContactUsHeader = styled(ScrollHeader1)`
   padding-top: 50px !important;
 `;
 
@@ -58,6 +55,10 @@ const WarningMessage = styled(Message)`
 
 const MaxWidthP = styled(StyledP)`
   max-width: 675px;
+`;
+
+const PaddedDiv = styled.div`
+  padding: 0 30px 0 30px;
 `;
 
 export default function Home({ canSubmit }) {
@@ -77,142 +78,146 @@ export default function Home({ canSubmit }) {
           </WarningMessage>
         )}
         <Banner />
-        <Header2>On this page:</Header2>
-        <Grid columns={2} stackable>
-          <Grid.Column>
-            <NavigationUl>
-              <li>
-                <a href="#program-explanation">How the program works</a>
-              </li>
-              <li>
-                <a href="#application-deadlines">Application deadlines</a>
-              </li>
-              <li>
-                <a href="#eligibility">Determine your eligibility</a>
-              </li>
-              <li>
-                <a href="#submit-application">Submit your application</a>
-              </li>
-              <li>
-                <HrefLink href="/files/program_guide.pdf" blank>
-                  Program guide
-                </HrefLink>
-              </li>
-              <li>
-                <HrefLink href="/faq">Frequently asked questions</HrefLink>
-              </li>
-              <li>
-                <a href="#contact">Contact us</a>
-              </li>
-            </NavigationUl>
-          </Grid.Column>
-          <Grid.Column>
-            <SBCLogo />
-          </Grid.Column>
-        </Grid>
+        <PaddedDiv>
+          <Header2>On this page:</Header2>
+          <Grid columns={2} stackable>
+            <Grid.Column>
+              <NavigationUl>
+                <li>
+                  <a href="#program-explanation">How the program works</a>
+                </li>
+                <li>
+                  <a href="#application-deadlines">Application deadlines</a>
+                </li>
+                <li>
+                  <a href="#eligibility">Determine your eligibility</a>
+                </li>
+                <li>
+                  <a href="#submit-application">Submit your application</a>
+                </li>
+                <li>
+                  <HrefLink href="/files/program_guide.pdf" blank>
+                    Program guide
+                  </HrefLink>
+                </li>
+                <li>
+                  <HrefLink href="/faq">Frequently asked questions</HrefLink>
+                </li>
+                <li>
+                  <a href="#contact">Contact us</a>
+                </li>
+              </NavigationUl>
+            </Grid.Column>
+            <Grid.Column>
+              <SBCLogo />
+            </Grid.Column>
+          </Grid>
 
-        <ScrollHeader1 id="program-explanation">How the program works</ScrollHeader1>
-        <MaxWidthP>
-          The Launch Online Grant program provides funding to B.C.-based business to create an online shop and/or
-          improve their e-commerce experience to attract new local customers and expand to new markets. The grant will
-          pay for up to 75% of eligible expenses, up to a maximum of $7,500 per business.
-        </MaxWidthP>
-        <MaxWidthP>
-          The program supports a rapid response for businesses and is accepting applications from businesses ready to
-          start and finish their online shop within 12 weeks.
-        </MaxWidthP>
-        <StyledUl>
-          <li>
-            Up-to 25% of funds will be reserved for Indigenous businesses and businesses operating outside of the lower
-            mainland and greater Victoria.
-          </li>
-          <li>The funds must be used to hire B.C.-based company(ies) to do the online store development.</li>
-        </StyledUl>
-
-        <ProgramSteps />
-        <BackToTop />
-
-        <ScrollHeader1 id="application-deadlines">Application deadlines</ScrollHeader1>
-        <MaxWidthP>
-          Program application intake is open until the funds have been fully subscribed. Funding will be awarded on a
-          first-come first-served basis to small- and medium-sized B.C. businesses.
-        </MaxWidthP>
-        <MaxWidthP>
-          For the projects to have an immediate benefit, the funds will be given as soon as a business has been approved
-          for the program.
-        </MaxWidthP>
-        <BackToTop />
-
-        <ScrollHeader1 id="eligibility">Determine your eligibility</ScrollHeader1>
-        <MaxWidthP>Review the following criteria to determine if your business is eligible for the program.</MaxWidthP>
-
-        <EligibilityCriteria />
-        <BackToTop />
-
-        <ScrollHeader1 id="submit-application">Submit your application</ScrollHeader1>
-
-        <Header2>Before applying</Header2>
-        <MaxWidthP>
-          Please review the eligibility information on this page. If you have any questions, please use the contact form
-          at the bottom of this page for support before submitting your application. Incomplete applications cannot be
-          approved to receive a grant.
-        </MaxWidthP>
-        <MaxWidthP>
-          Prepare a proposal for how much grant funding you need and how you plan to use the funds, with estimates for
-          the eligible expenses. The application form will contain a space for you to include your proposal (note the
-          max size is 1,000 characters) and cost estimates.
-        </MaxWidthP>
-
-        <Header2>You will be asked to provide:</Header2>
-        <StyledUl>
-          <li>Current year balance or past fiscal year statement</li>
-          <li>Proof of a valid B.C. business licence</li>
-          <li>Business number</li>
-          <li>PST and WorkSafeBC registrations, if applicable</li>
-          <li>Income tax return:</li>
+          <ScrollHeader1 id="program-explanation">How the program works</ScrollHeader1>
+          <MaxWidthP>
+            The Launch Online Grant program provides funding to B.C.-based business to create an online shop and/or
+            improve their e-commerce experience to attract new local customers and expand to new markets. The grant will
+            pay for up to 75% of eligible expenses, up to a maximum of $7,500 per business.
+          </MaxWidthP>
+          <MaxWidthP>
+            The program supports a rapid response for businesses and is accepting applications from businesses ready to
+            start and finish their online shop within 12 weeks.
+          </MaxWidthP>
           <StyledUl>
-            <li>Notice of assessment (2019); or</li>
-            <li>First section of T2 return or first section of owner’s tax return (form 5010-R) (2019 or 2020)</li>
+            <li>
+              Up-to 25% of funds will be reserved for Indigenous businesses and businesses operating outside of the
+              lower mainland and greater Victoria.
+            </li>
+            <li>The funds must be used to hire B.C.-based company(ies) to do the online store development.</li>
           </StyledUl>
-          <li>B.C. business registration number and official registered name</li>
-        </StyledUl>
 
-        <InfoMessage
-          text={`Note: All above mentioned documents must be provided to Alacrity Canada if application is
-                  conditionally approved before funds can be disbursed.`}
-        />
+          <ProgramSteps />
+          <BackToTop />
 
-        <Header2>Receive funding to build your online store</Header2>
-        <MaxWidthP>
-          Successful applicants will receive funding within 2-3 weeks of approval and are required to develop or upgrade
-          their online shop within 12 weeks of receiving the grant.
-        </MaxWidthP>
+          <ScrollHeader1 id="application-deadlines">Application deadlines</ScrollHeader1>
+          <MaxWidthP>
+            Program application intake is open until the funds have been fully subscribed. Funding will be awarded on a
+            first-come first-served basis to small- and medium-sized B.C. businesses.
+          </MaxWidthP>
+          <MaxWidthP>
+            For the projects to have an immediate benefit, the funds will be given as soon as a business has been
+            approved for the program.
+          </MaxWidthP>
+          <BackToTop />
 
-        <GeneralInformation canSubmit={canSubmit} />
-        <BackToTop />
+          <ScrollHeader1 id="eligibility">Determine your eligibility</ScrollHeader1>
+          <MaxWidthP>
+            Review the following criteria to determine if your business is eligible for the program.
+          </MaxWidthP>
 
-        <ContactUsHeader id="contact">Contact Us</ContactUsHeader>
-        <MaxWidthP>
-          Please contact us directly to ask questions. We will respond within <strong>3 business days.</strong>
-        </MaxWidthP>
-        <div>
-          <Info>
-            <Icon name="phone" />
-            844-487-1266
-          </Info>
-        </div>
-        <div>
-          <Info>
-            <HrefLink href="mailto:info@launchonline.ca">
-              <Icon name="mail" />
-              info@launchonline.ca
-            </HrefLink>
-          </Info>
-        </div>
+          <EligibilityCriteria />
+          <BackToTop />
 
-        <HorizontalRule />
+          <ScrollHeader1 id="submit-application">Submit your application</ScrollHeader1>
 
-        <Acknowledgement />
+          <Header2>Before applying</Header2>
+          <MaxWidthP>
+            Please review the eligibility information on this page. If you have any questions, please use the contact
+            form at the bottom of this page for support before submitting your application. Incomplete applications
+            cannot be approved to receive a grant.
+          </MaxWidthP>
+          <MaxWidthP>
+            Prepare a proposal for how much grant funding you need and how you plan to use the funds, with estimates for
+            the eligible expenses. The application form will contain a space for you to include your proposal (note the
+            max size is 1,000 characters) and cost estimates.
+          </MaxWidthP>
+
+          <Header2>You will be asked to provide:</Header2>
+          <StyledUl>
+            <li>Current year balance or past fiscal year statement</li>
+            <li>Proof of a valid B.C. business licence</li>
+            <li>Business number</li>
+            <li>PST and WorkSafeBC registrations, if applicable</li>
+            <li>Income tax return:</li>
+            <StyledUl>
+              <li>Notice of assessment (2019); or</li>
+              <li>First section of T2 return or first section of owner’s tax return (form 5010-R) (2019 or 2020)</li>
+            </StyledUl>
+            <li>B.C. business registration number and official registered name</li>
+          </StyledUl>
+
+          <InfoMessage
+            text={`Note: All above mentioned documents must be provided to Alacrity Canada if application is
+                    conditionally approved before funds can be disbursed.`}
+          />
+
+          <Header2>Receive funding to build your online store</Header2>
+          <MaxWidthP>
+            Successful applicants will receive funding within 2-3 weeks of approval and are required to develop or
+            upgrade their online shop within 12 weeks of receiving the grant.
+          </MaxWidthP>
+
+          <GeneralInformation canSubmit={canSubmit} />
+          <BackToTop />
+
+          <ScrollHeader1 id="contact">Contact Us</ScrollHeader1>
+          <MaxWidthP>
+            Please contact us directly to ask questions. We will respond within <strong>3 business days.</strong>
+          </MaxWidthP>
+          <div>
+            <Info>
+              <Icon name="phone" />
+              844-487-1266
+            </Info>
+          </div>
+          <div>
+            <Info>
+              <HrefLink href="mailto:info@launchonline.ca">
+                <Icon name="mail" />
+                info@launchonline.ca
+              </HrefLink>
+            </Info>
+          </div>
+
+          <HorizontalRule />
+
+          <Acknowledgement />
+        </PaddedDiv>
       </Container>
     </>
   );
