61153c20895b4cd8209235a5e019a72f32e73c24
Jonathan Langlois <jon@button.is>
2021-03-17 08:49:27 -0700

    test: cypress test

--- app/cypress/fixtures/expectedResults.js
+++ app/cypress/fixtures/expectedResults.js
@@ -18,6 +18,12 @@ const childParentRelationships = {
 };
 const expectedResult = {};
 
+expectedResult.declarations = [
+  'I understand that grant funding received through this program must be used to support the development and improvement of online shop of the business this application identifies only.',
+  'I confirm that I understand that the personal information collected through this application process is collected for the administration of Launch Online Grant including to confirm residency, under s.26(c) of the Freedom of Information and Protection of Privacy Act. I also confirm that I have obtained authorization from the employees to whom the personal information relates to share that information with the Alacrity Canada for the above mentioned purposes. If you have questions about the collection you may contact info@launchonline.ca',
+  'I understand that the receipt of grants under this program may have implications under Canada’s Income Tax Act, administered by the federal government. I am responsible for obtaining appropriate advice with respect to my obligations under this legislation.',
+];
+
 function addResults(expectedResult, pageEntries) {
   pageEntries.inputs?.forEach(input => {
     const name = input.getBy.split('_')[1];
@@ -79,11 +85,5 @@ function addResults(expectedResult, pageEntries) {
 formFieldEntries.forEach(entry => {
   addResults(expectedResult, entry);
 });
-
-expectedResult.declarations = [
-  'I understand that grant funding received through this program must be used to support the development and improvement of online shop of the business this application identifies only.',
-  'I confirm that I understand that the personal information collected through this application process is collected for the administration of Launch Online Grant including to confirm residency, under s.26(c) of the Freedom of Information and Protection of Privacy Act. I also confirm that I have obtained authorization from the employees to whom the personal information relates to share that information with the Alacrity Canada for the above mentioned purposes. If you have questions about the collection you may contact info@launchonline.ca',
-  'I understand that the receipt of grants under this program may have implications under Canada’s Income Tax Act, administered by the federal government. I am responsible for obtaining appropriate advice with respect to my obligations under this legislation.',
-];
 
 export default expectedResult;
