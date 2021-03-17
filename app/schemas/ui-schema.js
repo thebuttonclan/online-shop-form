b1ba307e7fa7c86cbf465d51452f275e927e390b
Jonathan Langlois <jon@button.is>
2021-03-17 11:27:53 -0700

    chore: update hint text
    105

--- app/schemas/ui-schema.js
+++ app/schemas/ui-schema.js
@@ -27,7 +27,7 @@ const uiSchema = {
     'ui:help': 'Please use the format xxx-xxx-xxxx',
   },
   businessAddress: {
-    'ui:help': 'For example: 1111 Government St. A1A 1A1',
+    'ui:help': 'For example: 1111 Government St., Victoria BC, A1A 1A1',
   },
   pstNumber: {
     'ui:help': 'For example: PST-1234-5678',
@@ -108,6 +108,9 @@ const uiSchema = {
     },
     staffTrainingDescription: {
       'ui:widget': 'TextareaWidget',
+    },
+    lastPage: {
+      'ui:widget': 'checkbox',
     },
   },
 
