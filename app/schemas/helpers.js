ad1ee907b817d0e402ff31e61510cc9bf6a8bce6
Jonathan Langlois <jon@button.is>
2021-03-16 23:10:12 -0700

    fix: defaults for potentially undefined costs
    105

--- app/schemas/helpers.js
+++ app/schemas/helpers.js
@@ -2,7 +2,7 @@ import values from 'lodash/values';
 import sum from 'lodash/sum';
 
 export const calculateGrantAmount = formData => {
-  const { staffTrainingCosts, serviceProviderCosts, customerAcquisitionCosts } = formData.costs || {};
+  const { staffTrainingCosts = 0, serviceProviderCosts = 0, customerAcquisitionCosts = 0 } = formData.costs || {};
   const costs = [staffTrainingCosts, serviceProviderCosts, customerAcquisitionCosts];
   const total = sum(costs.map(Number));
   const grandAmount = total * 0.75 || 0;
