7b4796e53019ceafb47b3bab4c964ad7862c8fa9
Jonathan Langlois <jon@button.is>
2021-03-29 12:16:34 -0700

    feat: filenames
    112

--- helm/mailhog/helm-install.sh
+++ helm/mailhog/helm-install.sh
@@ -7,5 +7,5 @@ helm repo update
 kubectl create namespace dev-mailhog
 kubectl create namespace test-mailhog
 
-helm install mailhog codecentric/mailhog -n dev-mailhog -f values-dev.yaml
-helm install mailhog codecentric/mailhog -n test-mailhog -f values-test.yaml
+helm install mailhog codecentric/mailhog -n dev-mailhog -f values-dev.secret.yaml
+helm install mailhog codecentric/mailhog -n test-mailhog -f values-test.secret.yaml
