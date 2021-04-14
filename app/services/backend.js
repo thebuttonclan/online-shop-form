60b0cf767e9c5e036b8a1503457d9a1ccc02eb2b
Junmin Ahn <junmin@button.is>
2021-04-14 14:27:07 -0700

    feat: enable email confirmation

--- app/services/backend.js
+++ app/services/backend.js
@@ -28,16 +28,15 @@ export async function submitApplication({ req, newData, js }) {
     res.log = `${SUCCESSFUL_APPLICATION_MSG}: ${count}`;
 
     // intentionally not waiting for the email sent to unblock the response
-    // keep this commented until the SMTP server issues are resolved
-    // sendEmail({
-    //   subject: 'Launch Online - Application Received',
-    //   to: req.session.formData.email,
-    //   template: 'confirmation',
-    //   context: {
-    //     phoneNum: '844-487-1266',
-    //     siteUrl: 'https://launchonline.ca',
-    //   },
-    // });
+    sendEmail({
+      subject: 'Launch Online - Application Received',
+      to: req.session.formData.email,
+      template: 'confirmation',
+      context: {
+        phoneNum: '844-487-1266',
+        siteUrl: 'https://launchonline.ca',
+      },
+    });
 
     req.session.formData = {};
 
